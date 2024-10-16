12bd1e2055abfbfd5f560a379ef23f6e528a42cd
Yvem Senna <t_jefersons@hapvida.com.br>
2024-10-16 19:34:53 -0300

    views configuradas

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+@api_view(['GET'])
+def get_users(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+def databaseEmDjango():
+
+    data = User.objects.get(pk='yvem_nick')
+
+    data = User.objects.filter(user_age='28')
+
+    data = User.objects.exclude()
+
+    data.save()
+
+    data.delete()
