c1dbd367d99a8aad2c469c4c4d6a04808cc82040
Yvem Senna <t_jefersons@hapvida.com.br>
2024-10-16 23:09:20 -0300

    final, CRUD completo

--- api_rest/views.py
+++ api_rest/views.py
@@ -12,68 +12,78 @@ import json
 
 from . import funcoes as fn
 
+
+
 @api_view(['GET'])
 def get_users(request):
 
     if request.method == 'GET':
-        users = User.objects.all()
 
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+        users = User.objects.all()                          # Get all objects in User's database (It returns a queryset)
+
+        serializer = UserSerializer(users, many=True)       # Serialize the object data into json (Has a 'many' parameter cause it's a queryset)
+
+        return Response(serializer.data)                    # Return the serialized data
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+
+
+@api_view(['GET', 'PUT'])
 def get_by_nick(request, nick):
+
     try:
-
         user = User.objects.get(pk=nick)
-
     except:
-
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     if request.method == 'GET':
 
         serializer = UserSerializer(user)
-
         return Response(serializer.data)
-    
+
+    if request.method == 'PUT':
+
+        serializer = UserSerializer(user, data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
 
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+# CRUDZAO DA MASSA
 @api_view(['GET','POST','PUT','DELETE'])
 def user_manager(request):
+
+# ACESSOS
 
     if request.method == 'GET':
 
         try:
-            
             if request.GET['user']:                         # Check if there is a get parameter called 'user' (/?user=xxxx&...)
 
                 user_nickname = request.GET['user']         # Find get parameter
 
                 try:
-                    
-                    user = User.objects.get(pk=user_nickname) 
-                    # Get the object in database
+                    user = User.objects.get(pk=user_nickname)   # Get the object in database
                 except:
-                    
                     return Response(status=status.HTTP_404_NOT_FOUND)
 
                 serializer = UserSerializer(user)           # Serialize the object data into json
                 return Response(serializer.data)            # Return the serialized data
+
             else:
-                
-                return Response(status=status.HTTP_400_BAD_REQUEST) 
-        except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
             
+        except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
+    
+
 # CRIANDO DADOS
 
     if request.method == 'POST':
